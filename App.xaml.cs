dd6f63d76a54ca82269f209163668e7de3caf7be
dim0kkk <157678172+grymik0@users.noreply.github.com>
2026-09-12 15:40:24 +0300

    Initial commit

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace WinTime
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
